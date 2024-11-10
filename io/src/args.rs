4fa037459f33bfccf5a1cf676edb62846b4183ea
Ilya Bylich <ibylich@gmail.com>
2024-11-11 00:51:59 +0100

    fix exit codes

--- io/src/args.rs
+++ io/src/args.rs
@@ -21,30 +21,27 @@ pub(crate) fn parse_args() -> Result<()> {
     let args = Args::parse();
 
     match args {
+        Args::Start => {
+            // The only case in which we proceeed to booting app
+            return Ok(());
+        }
         Args::Generate { shell } => {
             let mut cmd = Args::command();
             generate(shell, &mut cmd, "layer-shell", &mut std::io::stdout());
-            std::process::exit(0);
-        }
-        Args::Start => {
-            // The only case in which we proceeed
-            Ok(())
         }
         Args::Exit => {
             IPC::send_to_running_instance(IPCMessage::Exit)?;
-            std::process::exit(1);
         }
         Args::Toggle {
             window_name: WindowName::Launcher,
         } => {
             IPC::send_to_running_instance(IPCMessage::ToggleLauncher)?;
-            std::process::exit(1);
         }
         Args::Toggle {
             window_name: WindowName::LogoutScreen,
         } => {
             IPC::send_to_running_instance(IPCMessage::ToggleLogoutScreen)?;
-            std::process::exit(1);
         }
     }
+    std::process::exit(0);
 }
