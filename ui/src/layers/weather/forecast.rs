04958a44178834d4edb44dae4cedeb90d175439a
Ilya Bylich <ibylich@gmail.com>
2024-11-29 19:54:11 +0100

    get rid of the maybeuninit nonsense

--- ui/src/layers/weather/forecast.rs
+++ ui/src/layers/weather/forecast.rs
@@ -1,6 +1,6 @@
 use crate::{
     layers::weather::codes::{weather_code_to_description, weather_code_to_icon},
-    widgets::weather::{DailyImages, DailyLabels, HourlyImages, HourlyLabels},
+    widgets::weather::{DailyRows, HourlyRows},
 };
 use gtk4::prelude::WidgetExt;
 use layer_shell_io::{subscribe, Event};
@@ -8,7 +8,7 @@ use layer_shell_io::{subscribe, Event};
 pub(crate) fn init() {
     subscribe(|event| {
         if let Event::WeatherForecast { daily, hourly } = event {
-            for ((label, image), weather) in HourlyLabels().iter().zip(HourlyImages()).zip(hourly) {
+            for ((label, image), weather) in HourlyRows().iter().zip(hourly) {
                 let text = format!("{}' {:>5.1}℃", weather.hour, weather.temperature,);
                 label.set_label(&text);
                 label.set_tooltip_text(Some(&weather_code_to_description(weather.code)));
@@ -16,7 +16,7 @@ pub(crate) fn init() {
                 image.set_from_gicon(weather_code_to_icon(weather.code));
             }
 
-            for ((label, image), weather) in DailyLabels().iter().zip(DailyImages()).zip(daily) {
+            for ((label, image), weather) in DailyRows().iter().zip(daily) {
                 let text = format!(
                     "{}: {:>5.1}℃ - {:>5.1}℃",
                     weather.day, weather.temperature.start, weather.temperature.end,
--- ui/src/widgets/weather.rs
+++ ui/src/widgets/weather.rs
@@ -1,14 +1,11 @@
 use crate::widgets::widget;
 use gtk4::prelude::{BoxExt, GtkWindowExt, WidgetExt};
-use std::mem::MaybeUninit;
 
 widget!(Window, gtk4::Window);
 const HOURS: usize = 10;
-widget!(HourlyLabels, [gtk4::Label; HOURS]);
-widget!(HourlyImages, [gtk4::Image; HOURS]);
+widget!(HourlyRows, [(gtk4::Label, gtk4::Image); HOURS]);
 const DAYS: usize = 6;
-widget!(DailyLabels, [gtk4::Label; DAYS]);
-widget!(DailyImages, [gtk4::Image; DAYS]);
+widget!(DailyRows, [(gtk4::Label, gtk4::Image); DAYS]);
 
 pub(crate) fn setup() {
     let window = gtk4::Window::new();
@@ -23,55 +20,31 @@ pub(crate) fn setup() {
     layout.append(&left_side);
 
     left_side.append(&gtk4::Label::new(Some("Hourly")));
-    let mut hourly_images: [MaybeUninit<gtk4::Image>; HOURS] =
-        [const { MaybeUninit::uninit() }; HOURS];
-    let mut hourly_labels: [MaybeUninit<gtk4::Label>; HOURS] =
-        [const { MaybeUninit::uninit() }; HOURS];
-    for i in 0..HOURS {
+    let hourly_rows = std::array::from_fn::<_, HOURS, _>(|_| {
         let (row, label, image) = weather_row();
         left_side.append(&row);
-        hourly_labels[i].write(label);
-        hourly_images[i].write(image);
-    }
+        (label, image)
+    });
 
     let right_side = gtk4::Box::new(gtk4::Orientation::Vertical, 0);
     right_side.add_css_class("weather-right-side");
     layout.append(&right_side);
 
     right_side.append(&gtk4::Label::new(Some("Daily")));
-    let mut daily_images: [MaybeUninit<gtk4::Image>; DAYS] =
-        [const { MaybeUninit::uninit() }; DAYS];
-    let mut daily_labels: [MaybeUninit<gtk4::Label>; DAYS] =
-        [const { MaybeUninit::uninit() }; DAYS];
-    for i in 0..DAYS {
+    let daily_rows = std::array::from_fn::<_, DAYS, _>(|_| {
         let (row, label, image) = weather_row();
         right_side.append(&row);
-        daily_labels[i].write(label);
-        daily_images[i].write(image);
-    }
+        (label, image)
+    });
 
     set_Window(window);
-    set_HourlyLabels(unsafe {
-        std::mem::transmute::<[MaybeUninit<gtk4::Label>; HOURS], [gtk4::Label; HOURS]>(
-            hourly_labels,
-        )
-    });
-    set_HourlyImages(unsafe {
-        std::mem::transmute::<[MaybeUninit<gtk4::Image>; HOURS], [gtk4::Image; HOURS]>(
-            hourly_images,
-        )
-    });
-    set_DailyLabels(unsafe {
-        std::mem::transmute::<[MaybeUninit<gtk4::Label>; DAYS], [gtk4::Label; DAYS]>(daily_labels)
-    });
-    set_DailyImages(unsafe {
-        std::mem::transmute::<[MaybeUninit<gtk4::Image>; DAYS], [gtk4::Image; DAYS]>(daily_images)
-    });
+    set_HourlyRows(hourly_rows);
+    set_DailyRows(daily_rows);
 }
 
 fn weather_row() -> (gtk4::Box, gtk4::Label, gtk4::Image) {
     let row = gtk4::Box::new(gtk4::Orientation::Horizontal, 0);
-    let label = gtk4::Label::new(None);
+    let label = gtk4::Label::new(Some("..."));
     let image = gtk4::Image::new();
     image.set_pixel_size(24);
     row.append(&label);
