fe87d63222769d9f9931c724471eaabb0e757662
Ilya Bylich <ibylich@gmail.com>
2025-02-04 00:13:59 +0100

    remove http client crate

--- src/modules/weather/client.rs
+++ src/modules/weather/client.rs
@@ -1,23 +1,73 @@
+use std::{
+    io::{Read, Write},
+    net::{SocketAddr, TcpStream, ToSocketAddrs},
+    time::Duration,
+};
+
 use anyhow::{bail, Context, Result};
-use http_req::{request::Request, uri::Uri};
 
 pub(crate) fn get_weather() -> Result<Response> {
-    let uri = format!("https://api.open-meteo.com/v1/forecast?latitude=52.2298&longitude=21.0118&current={CURRENT_FIELDS}&hourly={HOURLY_FIELDS}&daily={DAILY_FIELDS}&timezone=Europe/Warsaw");
-    let uri = Uri::try_from(uri.as_str()).context("invalid URI")?;
-    let mut req = Request::new(&uri);
-    let mut body = vec![];
-    let res = req.send(&mut body).context("failed to send request")?;
-    let body = String::from_utf8(body).context("non-utf8 response body")?;
+    let json = get_weather_response().context("failed to get weather")?;
 
-    if !res.status_code().is_success() {
-        bail!(
-            "Failed to send weather request\ncode: {:?}\nbody: {:?}",
-            res.status_code(),
-            body
-        );
+    miniserde::json::from_str(&json).context("failed to parse response body")
+}
+
+fn get_weather_response() -> Result<String> {
+    const HOST: &str = "api.open-meteo.com";
+    const PATH: &str = concat!(
+        "/v1/forecast",
+        "?",
+        "latitude=52.2298",
+        "&",
+        "longitude=21.0118",
+        "&",
+        "current=temperature_2m,weather_code",
+        "&",
+        "hourly=temperature_2m,weather_code",
+        "&",
+        "daily=temperature_2m_min,temperature_2m_max,weather_code",
+        "&",
+        "timezone=Europe/Warsaw"
+    );
+
+    let addr = format!("{HOST}:80")
+        .to_socket_addrs()
+        .context("invalid host:port")?
+        .find(|addr| matches!(addr, SocketAddr::V4(_)))
+        .context("failed to resolve ipv4 of DNS name")?;
+
+    let mut socket = TcpStream::connect_timeout(&addr, Duration::from_secs(2))
+        .context("failed to open TCP stream")?;
+    socket
+        .set_write_timeout(Some(Duration::from_secs(2)))
+        .context("failed to set write timeout")?;
+    socket
+        .set_read_timeout(Some(Duration::from_secs(2)))
+        .context("failed to set read timeout")?;
+
+    let request = format!("GET {PATH} HTTP/1.0\r\nHost: {HOST}\r\nConnection: close\r\n\r\n");
+    socket
+        .write_all(request.as_bytes())
+        .context("failed to write")?;
+
+    let mut response = vec![];
+    socket
+        .read_to_end(&mut response)
+        .context("failed to read from socket")?;
+    let response = String::from_utf8(response).context("non-utf8 response")?;
+    let (headers, body) = response
+        .split_once("\r\n\r\n")
+        .context("malformed response")?;
+    let (line1, headers) = headers.split_once("\r\n").context("malformed response")?;
+    let status = line1
+        .strip_prefix("HTTP/1.1 ")
+        .context("malformed response")?;
+
+    if status != "200 OK" {
+        bail!("Failed to get weather, received non-200 response: {status}\n{headers}\n{response}");
     }
 
-    miniserde::json::from_str(&body).context("failed to parse response body")
+    Ok(body.to_string())
 }
 
 #[derive(miniserde::Deserialize, Debug)]
@@ -27,14 +77,12 @@ pub(crate) struct Response {
     pub(crate) daily: DailyResponse,
 }
 
-const CURRENT_FIELDS: &str = "temperature_2m,weather_code";
 #[derive(miniserde::Deserialize, Debug)]
 pub(crate) struct CurrentResponse {
     pub(crate) temperature_2m: f32,
     pub(crate) weather_code: u32,
 }
 
-const HOURLY_FIELDS: &str = "temperature_2m,weather_code";
 #[derive(miniserde::Deserialize, Debug)]
 pub(crate) struct HourlyResponse {
     pub(crate) time: Vec<String>,
@@ -42,7 +90,6 @@ pub(crate) struct HourlyResponse {
     pub(crate) weather_code: Vec<u32>,
 }
 
-const DAILY_FIELDS: &str = "temperature_2m_min,temperature_2m_max,weather_code";
 #[derive(miniserde::Deserialize, Debug)]
 pub(crate) struct DailyResponse {
     pub(crate) time: Vec<String>,
