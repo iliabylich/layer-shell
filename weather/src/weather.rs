25ebeaf717d0bdf8d6fda27a2e08523be1ad5f03
Ilya Bylich <ibylich@gmail.com>
2025-07-15 17:55:20 +0200

    convert weather to module

--- weather/src/weather.rs
+++ weather/src/weather.rs
@@ -1,35 +1,32 @@
 use crate::{WeatherEvent, client::Client};
 use anyhow::Result;
-use futures::Stream;
-use pin_project_lite::pin_project;
+use module::Module;
 use std::time::Duration;
-use tokio::{
-    sync::mpsc::{UnboundedReceiver, UnboundedSender},
-    task::JoinHandle,
-};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio_util::sync::CancellationToken;
 
-pin_project! {
-    pub struct Weather {
-        #[pin]
-        rx: UnboundedReceiver<WeatherEvent>,
-    }
+pub struct Weather {
+    etx: UnboundedSender<WeatherEvent>,
+    token: CancellationToken,
 }
 
-const NAME: &str = "Weather";
+#[async_trait::async_trait]
+impl Module for Weather {
+    const NAME: &str = "Weather";
 
-impl Weather {
-    pub fn spawn(token: CancellationToken) -> (&'static str, Self, JoinHandle<()>, ()) {
-        let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<WeatherEvent>();
-        let handle = tokio::task::spawn(async move {
-            if let Err(err) = Self::r#loop(tx, token).await {
-                log::error!(target: "Weather", "{err:?}");
-            }
-        });
-        (NAME, Self { rx }, handle, ())
+    type Event = WeatherEvent;
+    type Command = ();
+    type Ctl = ();
+
+    fn new(
+        etx: UnboundedSender<Self::Event>,
+        _: UnboundedReceiver<Self::Command>,
+        token: CancellationToken,
+    ) -> Self {
+        Self { etx, token }
     }
 
-    async fn r#loop(tx: UnboundedSender<WeatherEvent>, token: CancellationToken) -> Result<()> {
+    async fn start(&mut self) -> Result<()> {
         let mut timer = tokio::time::interval(Duration::from_secs(120));
         let client = Client::new()?;
 
@@ -38,27 +35,16 @@ impl Weather {
                 _ = timer.tick() => {
                     let events = get_weather(&client).await;
                     for event in events {
-                        tx.send(event)?;
+                        self.etx.send(event)?;
                     }
                 }
 
-                _ = token.cancelled() => {
+                _ = self.token.cancelled() => {
                     log::info!(target: "Weather", "exiting...");
                     return Ok(())
                 }
             }
         }
-    }
-}
-
-impl Stream for Weather {
-    type Item = WeatherEvent;
-
-    fn poll_next(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Option<Self::Item>> {
-        self.project().rx.poll_recv(cx)
     }
 }
 
