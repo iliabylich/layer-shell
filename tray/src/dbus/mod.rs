42f3b63ea22fe88cfa7783bdbe665e44d93f08ac
Ilya Bylich <ibylich@gmail.com>
2026-02-08 17:58:21 +0100

    remove previous implementation of the tray module

--- tray/src/dbus/mod.rs
+++
@@ -1,5 +0,0 @@
-mod name_lost;
-mod name_owner_changed;
-
-pub(crate) use name_lost::NameLost;
-pub(crate) use name_owner_changed::NameOwnerChanged;
--- tray/src/status_notifier_item/mod.rs
+++
@@ -1,9 +0,0 @@
-mod icon_name;
-mod icon_pixmap;
-mod menu;
-mod new_icon;
-
-pub(crate) use icon_name::IconName;
-pub(crate) use icon_pixmap::IconPixmap;
-pub(crate) use menu::Menu;
-pub(crate) use new_icon::NewIcon;
