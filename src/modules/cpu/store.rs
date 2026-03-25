8d6a328ec2d5971cb11c63b4e8b21c1942ee33b1
Ilya Bylich <ibylich@gmail.com>
2026-03-26 00:01:48 +0100

    use report_and_exit instead of assert

--- src/modules/cpu/store.rs
+++ src/modules/cpu/store.rs
@@ -1,3 +1,4 @@
+use crate::macros::report_and_exit;
 pub(crate) struct Store(Option<Vec<(u8, u64, u64)>>);
 
 impl Store {
@@ -7,21 +8,21 @@ impl Store {
 
     pub(crate) fn update(&mut self, next: Vec<(u8, u64, u64)>) -> Vec<u8> {
         let usage_per_core = if let Some(previous) = self.0.take() {
-            assert!(
-                previous.len() == next.len(),
-                "number of CPU cores has changed from {} to {} (bug?)",
-                previous.len(),
-                next.len()
-            );
+            if previous.len() != next.len() {
+                report_and_exit!(
+                    "number of CPU cores has changed from {} to {} (bug?)",
+                    previous.len(),
+                    next.len()
+                );
+            }
 
             fn load_comparing_to(
                 (next_id, next_idle, next_total): (u8, u64, u64),
                 (prev_id, prev_idle, prev_total): (u8, u64, u64),
             ) -> u8 {
-                assert!(
-                    next_id == prev_id,
-                    "CPU id mismatch: {next_id} vs {prev_id}",
-                );
+                if next_id != prev_id {
+                    report_and_exit!("CPU id mismatch: {next_id} vs {prev_id}",);
+                }
 
                 let idle_d = (next_idle - prev_idle) as f64;
                 let total_d = (next_total - prev_total) as f64;
